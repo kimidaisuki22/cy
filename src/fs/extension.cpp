0c5fe874da734bd0d8a7a021e922497c1e70aab2
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-07-27 22:03:10 +0800

    fix function.

--- src/fs/extension.cpp
+++ src/fs/extension.cpp
@@ -2,5 +2,8 @@
 std::filesystem::path cy::fs::change_extension(std::filesystem::path file_name,
                                                std::filesystem::path ext_name) {
   auto ext = std::filesystem::path(ext_name).extension();
+  if(ext_name.has_extension()){
+    ext_name = ext_name.extension();
+  }
   return file_name.replace_extension(ext_name);
 }
