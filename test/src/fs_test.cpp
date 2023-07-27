f2731ff94bbbaf5864d172e6601917b0ac9eebf4
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-07-27 22:03:15 +0800

    add gtest.

---
+++ test/src/fs_test.cpp
@@ -0,0 +1,19 @@
+#include "cy/fs/extension.h"
+#include <cy/fs.h>
+
+#include <gtest/gtest.h>
+
+TEST(Filesytem, replace_file_extension) {
+    EXPECT_EQ(cy::fs::change_extension("main.txt", ".cpp"), "main.cpp");
+    EXPECT_EQ(cy::fs::change_extension("main.txt", "main.cpp"), "main.cpp");
+    EXPECT_EQ(cy::fs::change_extension("main.txt", ".h"), "main.h");
+
+    EXPECT_EQ(cy::fs::change_extension(".config", ".cpp"), ".config.cpp");
+    EXPECT_EQ(cy::fs::change_extension(".config", "a.cpp"), ".config.cpp");
+
+    EXPECT_NE(cy::fs::change_extension("main.cpp", ".txt"), "main.cpp");
+}
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
