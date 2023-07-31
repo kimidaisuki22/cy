2f97341b9659db9d722b754783f325424992548f
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-07-31 22:26:50 +0800

    change example.

--- example/src/proc_example.cpp
+++ example/src/proc_example.cpp
@@ -1,13 +1,22 @@
+#include <algorithm>
 #include <cy/proc/executable.h>
+#include <cy/text/process.h>
 #include <iostream>
 
 int main() {
   std::cout << cy::proc::get_executable_path() << std::endl;
   {
-    auto s = cy::proc::execute_command("help");
-
-    std::cout << s.size() << " help: ";
-    s.resize(80);
-    std::cout << s << std::endl;
+    auto command =
+#ifdef _WIN32
+        "ipconfig";
+#else
+        "ip a";
+#endif
+    auto lines = cy::text::to_lines(cy::proc::execute_command(command));
+    std::sort(lines.begin(), lines.end());
+    for (auto line : lines) {
+      if (cy::text::contains(line, "inet") || cy::text::contains(line, "IPv"))
+        std::cout << cy::text::trim(line) << std::endl;
+    }
   }
 }
