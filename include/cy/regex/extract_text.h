3d7cec0d96485c85165dcf485dff06eb5933854f
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-10-15 15:40:51 +0800

    add multi extract and example.

--- include/cy/regex/extract_text.h
+++ include/cy/regex/extract_text.h
@@ -2,10 +2,15 @@
 #include <optional>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace cy::regex {
 
 std::optional<std::string>
 extract_one_string_from_pattern(const std::string &src,
                                 const std::string &pattern);
+
+std::optional<std::vector<std::string>>
+extract_strings_from_pattern(const std::string &src,
+                             const std::string &pattern);
 } // namespace cy::regex
--- src/regex/extract_text.cpp
+++ src/regex/extract_text.cpp
@@ -1,6 +1,9 @@
+#include <cstddef>
 #include <cy/regex/extract_text.h>
 #include <optional>
 #include <regex>
+#include <string>
+#include <vector>
 
 namespace cy::regex {
 std::optional<std::string>
@@ -15,4 +18,19 @@ extract_one_string_from_pattern(const std::string &src,
   }
   return {};
 }
+std::optional<std::vector<std::string>>
+extract_strings_from_pattern(const std::string &src,
+                             const std::string &pattern) {
+                                std::regex regex(pattern);
+  std::smatch match;
+
+  if (std::regex_search(src, match, regex) && match.size() > 1) {
+    std::vector<std::string> results;
+    for(size_t i{1};i<match.size();i++){
+      results.push_back(match[i].str());
+    }
+    return results;
+  }
+  return {};
+                             }
 } // namespace cy::regex
