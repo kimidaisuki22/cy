29ccb5084c679f9f9dce55d9a8b93bf103271832
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-02-23 20:04:48 +0800

    add new dir iterate.

---
+++ test/src/experiment/fs_test.cpp
@@ -0,0 +1,27 @@
+#include "cy/experiment/fs/iterate_dir.h"
+
+#include <cstddef>
+#include <filesystem>
+#include <gtest/gtest.h>
+
+TEST(Fs, lazy_collect) {
+  size_t count{};
+  for (auto file :
+       cy::experiment::fs::iterate_files_in_dir_non_recursive(".")) {
+    auto path = file.path();
+    EXPECT_NE(path.string(), "a");
+    count++;
+  }
+  EXPECT_GE(count, 0);
+}
+TEST(Fs, recursive_iteration) {
+  bool hint{};
+  auto self = std::filesystem::path(__FILE__);
+  for (auto file : cy::experiment::fs::iterate_files_in_dir_recursive(
+          self / "..")) {
+   if(std::filesystem::absolute(file) == self){
+    hint = true;
+   }
+  }
+  EXPECT_TRUE(hint);
+}
