96323888a44180cf9a100d6e8621b91e6a15a365
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-03-17 18:08:07 +0800

    make task awaitable.

--- include/cy/experiment/coroutine/simple_task.h
+++ include/cy/experiment/coroutine/simple_task.h
@@ -12,11 +12,23 @@ public:
 
   struct promise_type {
     std::exception_ptr exception_;
+    T result_value_;
+    std::coroutine_handle<> pending_coroutine_{};
 
     std::suspend_never initial_suspend() { return {}; }
-    std::suspend_never final_suspend() noexcept { return {}; }
-    Simple_task get_return_object() { return Simple_task{handle_type::from_promise(*this)}; }
-    void return_value(T returned_value) {}
+    std::suspend_never final_suspend() noexcept {
+      if (pending_coroutine_) {
+        pending_coroutine_.resume();
+      }
+      return {};
+    }
+    Simple_task get_return_object() {
+      return Simple_task{handle_type::from_promise(*this)};
+    }
+    // return value is called before the final suspend.
+    void return_value(T returned_value) {
+      result_value_ = std::move(returned_value);
+    }
     void unhandled_exception() { exception_ = std::current_exception(); }
   };
 
@@ -25,6 +37,19 @@ public:
   Simple_task &operator=(const Simple_task &) = delete;
   Simple_task(Simple_task &&o) noexcept : coro(std::exchange(o.coro, {})) {}
   ~Simple_task() {}
+
+  auto operator co_await() {
+    struct awaitable {
+      Simple_task *self_;
+      T *result_value_{};
+      bool await_ready() { return false; }
+      void await_suspend(std::coroutine_handle<> h) {
+        self_->coro.promise().pending_coroutine_ = h;
+      }
+      T await_resume() { return std::move(*result_value_); }
+    };
+    return awaitable{this, &coro.promise().result_value_};
+  }
 
 private:
   handle_type coro;
--- test/src/experiment/coroutine_test.cpp
+++ test/src/experiment/coroutine_test.cpp
@@ -4,7 +4,10 @@
 #include "cy/experiment/coroutine/simple_generator.h"
 #include "cy/experiment/coroutine/simple_task.h"
 
+#include <atomic>
+#include <chrono>
 #include <gtest/gtest.h>
+#include <thread>
 #include <vector>
 
 cy::experiment::coroutine::Simple_generator<int> one_to_ten() {
@@ -12,9 +15,17 @@ cy::experiment::coroutine::Simple_generator<int> one_to_ten() {
     co_yield i;
   }
 }
+
+cy::experiment::coroutine::Simple_task<int>
+f_sleep(cy::experiment::coroutine::Scheduler_pack &schedulers, int second) {
+  co_await schedulers.await_io();
+  std::this_thread::sleep_for(std::chrono::seconds{second});
+  co_return second;
+}
+
 cy::experiment::coroutine::Simple_task<int>
 f_task(cy::experiment::coroutine::Scheduler_pack &schedulers, int index,
-       int &output) {
+       int &output, int &count_down) {
 
   co_await schedulers.await_io();
 
@@ -28,9 +39,13 @@ f_task(cy::experiment::coroutine::Scheduler_pack &schedulers, int index,
     ch.resize(1024);
     memset(ch.data(), 0, ch.size());
   }
+
+  int time = co_await f_sleep(schedulers, index / 10);
+
   co_await schedulers.await_main();
-  std::cout << "Done " << index << " \n";
+  std::cout << "Done " << index << " for " << time << " seconds\n";
   output += 1;
+  count_down--;
   co_return 0;
 }
 
@@ -48,12 +63,15 @@ TEST(Coroutine, simple_coroutine) {
 TEST(Coroutine, simple_task) {
   cy::experiment::coroutine::Scheduler_pack schedulers{10, 10};
   cy::experiment::coroutine::Scheduler_executor exe{schedulers};
+  const int task_count = 20;
+  std::atomic_int count_down{task_count};
+  int running = task_count;
   int output = 0;
-  for (int i = 0; i < 20; i++) {
-    f_task(schedulers, i, output);
+  for (int i = 0; i < task_count; i++) {
+    f_task(schedulers, i, output, running);
   }
-
-  for (int i = 0; !cy::experiment::coroutine::is_no_task(schedulers); i++) {
+  // when there is no remain task, there may have some tasks is running.
+  for (int i = 0; running; i++) {
     std::this_thread::sleep_for(std::chrono::seconds{1});
     std::cout << "Tick " << i << "\n";
     exe.main_task();
