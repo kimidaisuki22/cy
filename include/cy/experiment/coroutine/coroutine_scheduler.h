806cb32bbf2fc2d8f50300349cf99fd21a7d8b1c
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-03-17 17:10:39 +0800

    add coroutine scheduler.

---
+++ include/cy/experiment/coroutine/coroutine_scheduler.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <condition_variable>
+#include <coroutine>
+#include <deque>
+namespace cy::experiment::coroutine {
+class Coroutine_scheduler {
+public:
+  using Handle = std::coroutine_handle<>;
+  void add(Handle handle);
+  bool empty() const;
+  Handle pop();
+  void notify();
+  // if predict return true, it stops waiting.
+  void wait(auto stop_predict) {
+    std::unique_lock lock{mutex_};
+    wait_wake_up_.wait(lock, [&stop_predict, this] {
+      return stop_predict() || !handles_.empty();
+    });
+  }
+
+private:
+  std::deque<Handle> handles_{};
+  std::mutex mutex_{};
+  std::condition_variable wait_wake_up_{};
+};
+} // namespace cy::experiment::coroutine
