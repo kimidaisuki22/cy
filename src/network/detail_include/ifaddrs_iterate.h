9aa2ac159191f79beec7148812366c953c4678c3
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2024-04-01 10:23:43 -0400

    fix build on linux.

--- src/network/detail_include/ifaddrs_iterate.h
+++ src/network/detail_include/ifaddrs_iterate.h
@@ -1,6 +1,7 @@
 #pragma once
-#include <cy/experiment/coroutine/simple_generator.h>
 #ifndef  _WIN32
+#include <cy/experiment/coroutine/simple_generator.h>
+#include <ifaddrs.h>
 namespace cy::network {
 // This function is not working on Windows.
 cy::experiment::coroutine::Simple_generator<ifaddrs *> iterate_interface();
